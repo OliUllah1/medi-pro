3953bc9265fcc4323f63cd6c665e6c369d785bad
OliUllah1 <oliullah915@gmail.com>
2023-08-28 11:24:41 +0600

    footer section done

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,28 +2,28 @@ import Image from "next/image";
 import {FaArrowAltCircleRight } from "react-icons/fa";
 const Contact = () => {
     return (
-        <div className="relative">
-            <Image  className='' src="/home-contact-us-bg.png" alt="doctor" width="1400" height="1400" />
-            <div className="bg-gradient-to-r from-black to-slate-900 opacity-60 absolute top-0 left-0 w-full h-full">
+        <div className="relative px-3 lg:px-0 bg-slate-100">
+            <Image  className='hidden lg:block' src="/home-contact-us-bg.png" alt="doctor" width="1400" height="1400" />
+            <div className="bg-gradient-to-r from-black to-slate-900 opacity-60 absolute top-0 left-0 w-full hidden lg:block h-full">
             </div>
-            <div className="absolute top-0 w-full h-full">
-                <div className="text-white space-y-2 py-10 text-5xl mt-2  text-center">
+            <div className="lg:absolute top-0 w-full h-full">
+                <div className="text-black pb-10 lg:pb-0 lg:text-white space-y-2 lg:py-8 text-5xl lg:mt-2  text-center">
                 <h4 className="text-xl ">FILL THE FORM</h4>
                 <h1 className="text-5xl">Contact Form.</h1>
                 </div>
-                <div className="flex px-10">
-                    <div className="w-[550px] space-y-5 p-14 bg-[#f7a582]">
+                <div className="lg:flex mt-8 lg:px-28">
+                    <div className="w-full lg:w-[550px] space-y-5 p-14 bg-[#f7a582]">
                     <Image  className='' src="/contact-us-img.png" alt="doctor" width="250" height="300" />
                     <h1 className="font-semibold text-3xl text-white">Make <span className="font-bold text-4xl">Appointment</span> & Take Care Of Your Healthy Life</h1>
                     </div>
-                    <div className="w-full py-14 px-28 bg-[#07332f]">
+                    <div className="w-full py-12 px-3 lg:px-28 bg-[#07332f]">
                         <form className="space-y-7" action="">
-                            <div className="w-full flex gap-10">
-                            <input placeholder="Name" className="border py-3 rounded-md px-3 w-full text-white bg-[#07332f] border-white" type="text" />
+                            <div className="w-full lg:flex gap-10">
+                            <input placeholder="Name" className="border py-3 rounded-md px-3 mb-7 lg:mb-0 w-full text-white bg-[#07332f] border-white" type="text" />
                             <input placeholder="Email" className="border py-3 rounded-md px-3 w-full text-white bg-[#07332f] border-white" type="email" />
                             </div>
-                            <div className="w-full flex gap-10">
-                            <input placeholder="Your Phone" className="border py-3 rounded-md px-3 w-full text-white bg-[#07332f] border-white" type="text" />
+                            <div className="w-full lg:flex gap-10">
+                            <input placeholder="Your Phone" className="border py-3 rounded-md px-3 mb-7 lg:mb-0 w-full text-white bg-[#07332f] border-white" type="text" />
                             <input type='date' placeholder="Date" className="border py-3 rounded-md px-3 w-full text-white bg-[#07332f] border-white" />
                             </div>
                             <textarea placeholder="Your Message" className="border h-28 py-3 rounded-md px-3 w-full text-white bg-[#07332f] border-white" name="" id=""></textarea>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import Image from "next/image";
+import { FaFacebookF,FaInstagram,FaLinkedinIn,FaYoutube,FaTwitter,FaClock,FaEnvelopeOpenText,FaMapMarkerAlt,FaPhoneAlt,FaArrowAltCircleRight } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="bg-[#07332f] space-y-5  px-3 lg:px-10 py-20">
+            <div className='space-y-10 lg:space-y-0 lg:flex justify-between gap-10'>
+            <div className="space-y-5">
+            <Image className='' src="/logo.png" alt="logo" width="150" height="150" />
+            <p className="text-white text-lg">Our family-centered approach to healthcare <br className="hidden lg:block" /> ensures that each member of your family <br className="hidden lg:block" />  receives personalized attention.</p>
+            <div className="flex gap-5 items-center text-2xl text-[#f7a582]">
+                <FaFacebookF className=" cursor-pointer hover:text-white"></FaFacebookF>
+                <FaInstagram className=" cursor-pointer hover:text-white"></FaInstagram>
+                <FaLinkedinIn className=" cursor-pointer hover:text-white"></FaLinkedinIn>
+                <FaTwitter className=" cursor-pointer hover:text-white"></FaTwitter>
+                <FaYoutube className=" cursor-pointer hover:text-white"></FaYoutube>
+            </div>
+            </div>
+            <div className="space-y-8">
+                <h4 className="text-[#f7a582] text-2xl">Quick Links</h4>
+                <ul className="text-white space-y-3">
+                    <li className="hover:text-[#f7a582]"><a href="">Home</a></li>
+                    <li className="hover:text-[#f7a582]"><a href="">About Us</a></li>
+                    <li className="hover:text-[#f7a582]"><a href="">Doctors</a></li>
+                    <li className="hover:text-[#f7a582]"><a href="">Services</a></li>
+                    <li className="hover:text-[#f7a582]"><a href="">Contact Us</a></li>
+                </ul>
+            </div>
+            <div className="space-y-8">
+                <h4 className="text-[#f7a582] text-2xl">Contact Details</h4>
+                <ul className="text-white space-y-3">
+                    <li className=""><a href="" className="flex gap-2 items-center"><FaMapMarkerAlt className="text-[#f7a582] text-xl"></FaMapMarkerAlt> Dhaka,Bangladesh</a></li>
+                    <li><a href="" className="flex gap-2 items-center"><FaEnvelopeOpenText className="text-[#f7a582] text-xl"></FaEnvelopeOpenText> healthcare@gmail.com</a></li>
+                    <li><a href="" className="flex gap-2 items-center"><FaPhoneAlt className="text-[#f7a582] text-xl"></FaPhoneAlt> 134564874656</a></li>
+                    <li><a href="" className="flex gap-2 items-center"><FaClock className="text-[#f7a582] text-xl"></FaClock> 8 AM - 5 PM , Monday - Saturday</a></li>
+                    
+                </ul>
+            </div>
+            <div className="space-y-8">
+                <h4 className="text-[#f7a582] text-2xl">Newsletter</h4>
+                <div className="text-white space-y-4">
+                <h4 className="font-semibold text-xl">Subscribe To Our Newsletter</h4>
+                <p className="text-lg">Stay informed and never miss out on the <br className="hidden lg:block" /> latest news, health tips.</p>
+                <form className="relative" action="">
+                    <input className="border border-white w-full py-[19px] bg-[#07332f] rounded-md px-5 text-white" placeholder="Enter Your Email" type="text" />
+                    <button className="absolute top-1 border-2 border-[#f7a582] right-1 py-3 bg-[#f7a582] px-5 rounded-md flex gap-2 items-center font-bold text-lg hover:bg-[#07332f] hover:text-[#f7a582]" type="submit">Send <FaArrowAltCircleRight></FaArrowAltCircleRight></button>
+                </form>
+                </div>
+            </div>
+        </div>
+        <p className="text-center font-semibold text-xl text-white">Copyright 2023 &#169; <span className="text-[#f7a582] font-bold">MediPro</span> All Right Reserved.</p>
         </div>
     );
 };
