d4b9cf923c80c58f51187944f5edcc35aacdc3b3
OliUllah1 <oliullah915@gmail.com>
2023-09-23 20:53:20 +0600

    about router page done

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,6 +4,10 @@ import AboutUs from '../../../components/About/AboutUs';
 import AboutQuality from '../../../components/About/AboutQuality';
 import AboutServices from '../../../components/About/AboutServices';
 import FamilyServices from '../../../components/About/FamilyServices';
+import Appointments from '../../../components/Appointments';
+import Doctors from '../../../components/Appointments/Doctors';
+import Testimonial from '../../../components/Testimonial';
+import Blog from '../../../components/Blog';
 
 
 const AboutPage = () => {
@@ -14,6 +18,10 @@ const AboutPage = () => {
             <AboutQuality></AboutQuality>
             <AboutServices></AboutServices>
             <FamilyServices></FamilyServices>
+            <Appointments></Appointments>
+            <Doctors></Doctors>
+            <Testimonial></Testimonial>
+            <Blog></Blog>
         </div>
     );
 };
